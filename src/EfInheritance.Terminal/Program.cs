6a15665924b70781faff892585486517ebd2900f
hosseinzadeh <hosseinzadeh@HOSSEINZADEH-PC>
2023-11-26 19:04:31 +0330

    Complete tph scenario code

--- src/EfInheritance.Terminal/Program.cs
+++ src/EfInheritance.Terminal/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EfInheritance.EfCore;
+using Microsoft.EntityFrameworkCore;
+
+using var dbContext = new AppDbContext();
+try
+{
+    await dbContext.Database.MigrateAsync();
+    Console.WriteLine("Migration being complete !");
+}
+catch(Exception ex)
+{
+    Console.WriteLine("ooooooooops !");
+    Console.WriteLine(ex.Message);
+}
